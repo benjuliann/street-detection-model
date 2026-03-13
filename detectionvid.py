3d8543f2570d6356fd77b6191b2196c01c4c8394
launchland <benjulian.noel@gmail.com>
2026-03-13 08:52:27 -0600

    Update detectionvid.py

--- detectionvid.py
+++ detectionvid.py
@@ -5,7 +5,7 @@ model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 
 #results = model(source='https://www.youtube.com/watch?v=X8dVS1z-m58', show=True, conf=0.4, save=True)  # predict on an image
 
-for result in model(source='https://www.youtube.com/watch?v=X8dVS1z-m58', stream=True, conf=0.4, save=True):
+for result in model(source='https://www.youtube.com/watch?v=LwzrWnWxLsU', stream=True, conf=0.4, save=True):
     frame = result.plot()                          # get annotated frame
     small = cv2.resize(frame, (640, 360))          # resize display only (width, height)
     cv2.imshow('YOLOv8 Detection', small)
